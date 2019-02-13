8f2e3b8331be7afea5972b7b030eb1c0b9251264
vdmppv <ejafjadlajokudl@gmail.com>
2019-02-13 22:37:25 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+#test = "пидорас"
+#print("Пошёл нахуй " + test)
+
+import sys
+
+def main():
+    print "Hello, World!"
+    print "This is {name}".format(name = sys.argv[0])
+
+if __name__ == "__main__":
+    main()
